d9eddaf02ca6418bcf536a4fda66cc63f52ac972
dzinoviev <danzino21@gmail.com>
2020-07-03 09:07:52 +0300

    Build: header styles and mobile menu are ready

--- src/js/index.js
+++ src/js/index.js
@@ -8,18 +8,31 @@
      * Initial function that fires after DOM will be ready to be changed from JS
      */
     function init() {
+        initMenu();
+    }
+
+    function initMenu() {
         var menuEl = document.querySelector('#menu'),
             menuBtn = document.querySelector('a[href="#menu-btn"]');
-        console.log(menuEl,menuBtn );
         var menu = new MmenuLight(menuEl, "(max-width: 768px)");
 
-        var navigator = menu.navigation();
+        var navigator = menu.navigation({
+            title: '',
+            theme: 'dark',
+            selected: 'active'
+        });
         var drawer = menu.offcanvas();
 
         menuBtn.addEventListener('click', function(e) {
             e.preventDefault();
-            console.log('CLICK');
             drawer.open();
+        });
+
+        
+        document.querySelectorAll('.close-menu').forEach(function(el) {
+            el.addEventListener('click', function() {
+                drawer.close();
+            })
         });
     }
 
