73e74d3d4a9fb9365de26945f1565a640fe0e130
dzinoviev <danzino21@gmail.com>
2020-07-05 14:59:06 +0300

    Build: added layout for content part of the site

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,35 @@
-;(function() {
+;(function($) {
 
     // Event Listeners
     document.addEventListener('DOMContentLoaded', init);
+    document.addEventListener('resize', resize);
+    document.addEventListener('scroll', scroll);
+
+    // Global variables
+    var navHeight = 56;
+    var isScrolling = false;
 
     // Functions
     /**
      * Initial function that fires after DOM will be ready to be changed from JS
      */
     function init() {
+
         initMenu();
+        initMenuSmoothScroll();
+        initMenuSticky();
+    }
+
+    function resize() {
+        
+    }
+
+    function scroll() {
+        initMenuSticky();
     }
 
     function initMenu() {
-        var menuEl = document.querySelector('#menu'),
+        var menuEl = document.querySelector('nav.navbar'),
             menuBtn = document.querySelector('a[href="#menu-btn"]');
         var menu = new MmenuLight(menuEl, "(max-width: 768px)");
 
@@ -36,4 +53,29 @@
         });
     }
 
-})();
+    function initMenuSmoothScroll() {
+        $('a[data-scroll]').on('click', function() {
+            isScrolling = true;
+            $('html').animate({
+                scrollTop: $( $.attr(this, 'href') ).offset().top - navHeight
+            }, 1000);
+            // Fix for blinking
+            setTimeout(function() {
+                isScrolling = false;
+            }, 100);
+        });
+    }
+
+    function initMenuSticky() {
+        var headerHeight = $('header').outerHeight();
+        if (isScrolling) {
+            return;
+        }
+        if (window.scrollY >= (headerHeight - navHeight)) {
+            $('header').addClass('sticky');
+        } else {
+            $('header').removeClass('sticky');
+        }
+    }
+
+})(jQuery);
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 /**
  * Libs
  */
+//= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/mmenu-light/dist/mmenu-light.js
+//= ../../node_modules/smooth-scroll/dist/smooth-scroll.min.js
 
 /**
  * Main
