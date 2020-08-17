d96081c6d7e51ec1bd89528f0cceb9e835664e27
dzinoviev <danzino21@gmail.com>
2020-08-17 20:49:46 +0300

    build: improved perdormance

--- gulpfile.js
+++ gulpfile.js
@@ -55,20 +55,20 @@ gulp.task('html:build', async function() {
 gulp.task('js:build', async function() {
 	gulp.src(path.src.js)
 			.pipe(rigger())
-			.pipe(sourcemaps.init())
+			// .pipe(sourcemaps.init())
 			.pipe(uglify())
-			.pipe(sourcemaps.write())
+			// .pipe(sourcemaps.write())
 			.pipe(gulp.dest(path.build.js))
 			.pipe(reload({stream: true}));
 });
 
 gulp.task('style:build', async function() {
 	gulp.src(path.src.style)
-			.pipe(sourcemaps.init())
+			// .pipe(sourcemaps.init())
 			.pipe(sass())
 			.pipe(prefixer())
 			.pipe(cssmin())
-			.pipe(sourcemaps.write())
+			// .pipe(sourcemaps.write())
 			.pipe(gulp.dest(path.build.css))
 			.pipe(reload({stream: true}));
 });
